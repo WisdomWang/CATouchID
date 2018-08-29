308296e71ecfc12b3103b305536df688ffc08ad2
Your Name <you@example.com>
2018-08-29 14:31:35 +0800

    first commit

---
+++ CATouchID/CATouchID.swift
@@ -0,0 +1,13 @@
+//
+//  CATouchID.swift
+//  CATouchID
+//
+//  Created by Cary on 2018/8/27.
+//  Copyright © 2018年 Cary. All rights reserved.
+//
+
+import UIKit
+
+class CATouchID: LAContext {
+
+}
